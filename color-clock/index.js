2cbf5af3278395a27edb6cfb6977d78af35bd069
codinci <dkibau@gmail.com>
2022-09-19 21:33:34 +0300

    Completed assignment

--- color-clock/index.js
+++ color-clock/index.js
@@ -10,7 +10,7 @@ function displayTime() {
     clock.style.color = "#" + Math.floor(Math.random() * 16777215).toString(16);
     clock.style.backgroundColor =
       "#" + Math.floor(Math.random() * 16777215).toString(16);
-  }, 1000);
+  }, 5000);
 }
 
 displayTime();
